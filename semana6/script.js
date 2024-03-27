b0d06e5c474dc7f73176d88e5080e4c9421ec883
Guilherme Tavares <149025209+taveres21@users.noreply.github.com>
2024-03-26 21:46:00 -0300

    Add files via upload

---
+++ semana6/script.js
@@ -0,0 +1,25 @@
+
+// DEFININDO ITENS
+// const calculator = document.querySelector('calculator');
+// const display = document.createElement('div');
+
+// DEFININDO ATRIBUTOS
+// display.id = "display";
+
+
+
+// MONTANDO CALCULADORA
+// calculator.appendChild(display);
+
+
+
+
+// LOGICA DA CALCULADORA
+// const itens = ['AC','+/-','%','/','7','8','9','x','4','5','6','-','1','2','3','+','0',',','='];
+
+// itens.forEach((item) => {
+//    let button = document.createElement('button');
+//    button.innerText = item;
+//    calculator.appendChild(button);
+//    document.body.appendChild(button);
+// });
